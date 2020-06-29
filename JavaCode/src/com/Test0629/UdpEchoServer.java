06518631a927d9a1a11647f77fa7d0312f29eac6
4ever_11 <734362064@qq.com>
2020-06-29 17:35:06 +0800

    UdpEchoClient

---
+++ JavaCode/src/com/Test0629/UdpEchoServer.java
@@ -0,0 +1,42 @@
+package com.Test0629;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UdpEchoServer {
+    private DatagramSocket socket = null;
+
+    public UdpEchoServer(int port) throws SocketException {
+        socket = new DatagramSocket(port);
+    }
+
+    public void start() throws IOException {
+        System.out.println("服务器启动!");
+        while (true){
+            DatagramPacket requestPacket = new DatagramPacket(new byte[4096],4096);
+            socket.receive(requestPacket);
+            String request = new String(requestPacket.getData(),
+                    0,requestPacket.getLength()).trim();
+            String response = process(request);
+
+            DatagramPacket responsePacket = new DatagramPacket(response.getBytes(),
+                    response.getBytes().length,requestPacket.getSocketAddress());
+            socket.send(responsePacket);
+            System.out.printf("[%s,%d]req:%s,resp:%s\n",requestPacket.getAddress().toString(),
+                    requestPacket.getPort(),request,response);
+            }
+        }
+
+
+    private String process(String request) {
+
+        return request;
+    }
+
+    public static void main(String[] args) throws IOException {
+        UdpEchoServer server = new UdpEchoServer(9090);
+        server.start();
+    }
+}
