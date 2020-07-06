12b922232b0a40e84f150b958be86067909e5675
4ever_11 <734362064@qq.com>
2020-07-06 18:09:23 +0800

    编程题 大整数排序

---
+++ JavaCode/src/com/Test0706/Main2.java
@@ -0,0 +1,22 @@
+package com.Test0706;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            int N = sc.nextInt();
+            BigInteger[] bigIntegers = new BigInteger[N];
+            for (int i = 0; i < N; i++) {
+                bigIntegers[i] = sc.nextBigInteger();
+            }
+            Arrays.sort(bigIntegers);
+            for (int i = 0; i < N; i++) {
+                System.out.println(bigIntegers[i]);
+            }
+        }
+    }
+}
