180192f69bbe3942eb63717699e3eb0e2d440fcc
4ever_11 <734362064@qq.com>
2020-07-12 09:47:14 +0800

    update

---
+++ java_maven_01/src/main/java/java0712/ServletDemo1.java
@@ -0,0 +1,22 @@
+package java0712;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Create with IntelliJ IDEA
+ * Description:
+ * User:Zyt
+ * Date:2020-07-12
+ */
+public class ServletDemo1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        String encoding =
+
+    }
+}
