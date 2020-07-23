5b40d7609bcaa850a2d45dea596855434dfc3913
4ever_11 <734362064@qq.com>
2020-07-23 21:53:46 +0800

    uodate

--- blog_2/src/main/java/api/LoginServlet.java
+++ blog_2/src/main/java/api/LoginServlet.java
@@ -1,11 +1,14 @@
 package api;
 
+import model.User;
+import model.UserDao;
 import view.HtmlGenerator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -16,7 +19,7 @@ import java.io.IOException;
  */
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
         //获取到用户名和密码，并进行简单的校验
         String name = req.getParameter("name");
@@ -28,5 +31,17 @@ public class LoginServlet extends HttpServlet {
             resp.getWriter().write(html);
             return;
         }
+        UserDao userDao = new UserDao();
+        User user = userDao.selectByName(name);
+        if (name == null || !password.equals(user.getPassword())){
+            String html = HtmlGenerator.getMessagePage("你输入的账号密码不匹配",
+                    "login.html");
+            resp.getWriter().write(html);
+            return;
+        }
+        HttpSession httpSession = req.getSession(true);
+        httpSession.setAttribute("user",user);
+        String html = HtmlGenerator.getMessagePage("登录成功","aticle");
+        resp.getWriter().write(html);
     }
 }
--- blog_2/src/main/java/api/RegisterServlet.java
+++ blog_2/src/main/java/api/RegisterServlet.java
@@ -1,10 +1,48 @@
 package api;
 
+import model.User;
+import model.UserDao;
+import view.HtmlGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Create with IntelliJ IDEA
  * Description:
  * User:Zyt
  * Date:2020-07-22
  */
-public class RegisterServlet {
+public class RegisterServlet  extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+        if (name == null || "".equals(name)
+                || password == null || "".equals(password)){
+            String html = HtmlGenerator.getMessagePage("用户名或密码为空",
+                    "register.html");
+            resp.getWriter().write(html);
+            return;
+        }
+        UserDao userDao = new UserDao();
+        User exitsUser = userDao.selectByName(name);
+        if (exitsUser != null){
+            String html = HtmlGenerator.getMessagePage("用户名已经存在",
+                    "register.html");
+            resp.getWriter().write(html);
+            return;
+        }
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.add(user);
+        String html = HtmlGenerator.getMessagePage("注册成功！点击跳转到登录页面",
+                "login.html");
+        resp.getWriter().write(html);
+    }
 }
