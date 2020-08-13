efedfc7874f2ac62bd533782959a3cc99060413c
4ever_11 <734362064@qq.com>
2020-08-13 23:05:21 +0800

    博客

--- blog_2/src/main/java/api/ArticleServlet.java
+++ blog_2/src/main/java/api/ArticleServlet.java
@@ -1,6 +1,9 @@
 package api;
 
+import model.Article;
+import model.ArticleDao;
 import model.User;
+import model.UserDao;
 import view.HtmlGenerator;
 
 import javax.servlet.ServletException;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Create with IntelliJ IDEA
@@ -29,6 +33,33 @@ public class ArticleServlet extends HttpServlet {
         }
         User user = (User)httpSession.getAttribute("user");
         //判断请求中是否存在 articleId 参数
+        String articleIdStr = req.getParameter("articleId");
+        if (articleIdStr == null){
+            getAllArticle(user,resp);
+        }else {
+            getOneArticle(Integer.parseInt(articleIdStr),user,resp);
+        }
+    }
 
+    private void getAllArticle(User user, HttpServletResponse resp) throws IOException {
+        ArticleDao articleDao = new ArticleDao();
+        List<Article> articles = articleDao.selectAll();
+        String html = HtmlGenerator.getArticleListPage(articles,user);
+        resp.getWriter().write(html);
+    }
+
+    private void getOneArticle(int articleId, User user, HttpServletResponse resp) throws IOException {
+        ArticleDao articleDao = new ArticleDao();
+        Article article = articleDao.selectById(articleId);
+        if (article == null){
+            String html = HtmlGenerator.getMessagePage("文章不存在","article");
+            resp.getWriter().write(html);
+            return;
+        }
+        UserDao userDao = new UserDao();
+        User author = userDao.selectById(article.getUserId());
+
+        String html = HtmlGenerator.getArticleDetailPage(article,user,author);
+        resp.getWriter().write(html);
     }
 }
--- blog_2/src/main/java/view/HtmlGenerator.java
+++ blog_2/src/main/java/view/HtmlGenerator.java
@@ -1,5 +1,10 @@
 package view;
 
+import model.Article;
+import model.User;
+
+import java.util.List;
+
 /**
  * Create with IntelliJ IDEA
  * Description:提示跳转页面
@@ -26,6 +31,80 @@ public class HtmlGenerator {
         stringBuilder.append(String.format("<a href=\"%s\"> 点击这里进行跳转 </a>",nextUrl));
         stringBuilder.append("</body>");
         stringBuilder.append("</html>");
+        return stringBuilder.toString();
+    }
+
+    public static String getArticleListPage(List<Article> articles, User user) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<html>");
+        stringBuilder.append("<head>");
+        stringBuilder.append("<meta charset=\"utf-8\">");
+        stringBuilder.append("<title>提示页面");
+        stringBuilder.append("<style>");
+        stringBuilder.append(".article {" +
+                "color: #333; " +
+                "test-decoration:none;" +
+                "width:200px;" +
+                "height: 50px;" +
+                "}");
+        stringBuilder.append(".artice:hover {" +
+                " color: white;" +
+                "backgroud-color:orange;" +
+                "}");
+        stringBuilder.append("body {" +
+                "backgroud-repeat:none;" +
+                "backgroud-position: 0 center;" +
+                "}");
+        stringBuilder.append("<style>");
+        stringBuilder.append("</head>");
+        stringBuilder.append("<html>");
+        stringBuilder.append("<h3>欢迎你！" + user.getName() + "</h3>");
+        stringBuilder.append("<hr>");
+        for (Article article : articles){
+            stringBuilder.append(String.format("<div stryle=\"width: 200px;heighr: 50px; line-height: 50px\"><a class=\"article\" " +
+                    "herf=\"article?articlId=%d\"> %s</a>)" + "<a href=\"deleteArticle?articleId\">删除</a></div>",article.getArticleId(),
+                    article.getTitle(),article.getArticleId()));
+        }
+            stringBuilder.append("<hr>");
+            stringBuilder.append(String.format("<div>当前共有博客 %d 篇</div>",articles.size()));
+
+            stringBuilder.append("<div>发表文章</div>");
+            stringBuilder.append("<div>");
+            stringBuilder.append("<form method=\"post\" action=\"article\">");
+            stringBuilder.append("<input type=\"test\" style\"width: 50px;height: 300px;\"></textarea>");
+            stringBuilder.append("<br>");
+            stringBuilder.append("<textarea nanme=\"content\" style=\"width: 50px;heigh: 300px;\"></textarea>");
+            stringBuilder.append("<br>");
+            stringBuilder.append("<input type=\"submit\" value\"发布文章\">");
+            stringBuilder.append("</from>");
+            stringBuilder.append("</div>");
+            stringBuilder.append("</body>");
+            stringBuilder.append("</html>");
+            return stringBuilder.toString();
+    }
+
+    public static String getArticleDetailPage(Article article, User user, User author) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<html>");
+        stringBuilder.append("<head>");
+        stringBuilder.append("<meta charset=\"utf-8\">");
+        stringBuilder.append("<title>提示页面</title>");
+        stringBuilder.append("<style>");
+        stringBuilder.append("a {" +
+                "color: #333;" +
+                "text-decoration: none;" +
+                "display: inline-block;" +
+                "width: 200px;" +
+                "height: 50px;" +
+                "}");
+        stringBuilder.append("a:hover {" +
+                "color: white;" +
+                "background-color: orange;" +
+                "}");
+        stringBuilder.append("<html>");
+        stringBuilder.append("<html>");
+        stringBuilder.append("<html>");
+
         return stringBuilder.toString();
     }
 }
