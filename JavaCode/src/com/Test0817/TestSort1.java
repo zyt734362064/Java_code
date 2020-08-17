1129b798a6f155765aacaefb1104ffcab5a7efed
4ever_11 <734362064@qq.com>
2020-08-17 23:37:40 +0800

    希尔排序

--- JavaCode/src/com/Test0817/TestSort1.java
+++ JavaCode/src/com/Test0817/TestSort1.java
@@ -28,9 +28,37 @@ public class TestSort1 {
         }
     }
 
+
+    public static void shellSort(int[] array){
+        int gap = array.length / 2;
+        while (gap > 1){
+            insertSortGap(array,gap);
+            gap = gap / 2;
+        }
+        insertSortGap(array,1);
+    }
+
+    private static void insertSortGap(int[] array, int gap) {
+
+        for (int bound = gap;bound < array.length;bound++){
+            int v = array[bound];
+            int cur = bound - gap;
+            while (cur >= 0){
+                if (array[cur] > v){
+                    array[cur + gap] = array[cur];
+                }else {
+                    break;
+                }
+                cur -= gap;
+            }
+            array[cur + gap] = v;
+        }
+    }
+
+
     public static void main(String[] args) {
         int[] array = {9,5,2,7,3,6,8};
-        insertSort(array);
+        shellSort(array);
         System.out.println(Arrays.toString(array));
     }
 }
---
+++ JavaCode/src/com/Test0817/TestSort2.java
@@ -0,0 +1,42 @@
+package com.Test0817;
+
+import java.util.Arrays;
+
+/**
+ * Create with IntelliJ IDEA
+ * Description:希尔排序
+ * User:Zyt
+ * Date:2020-08-17
+ */
+public class TestSort2 {
+    public static void shellSort(int[] array) {
+        int gap = array.length / 2;
+        while (gap > 1){
+            insertSortGap(array,gap);
+            gap = gap / 2;
+        }
+        insertSortGap(array,1);
+    }
+
+    private static void insertSortGap(int[] array, int gap) {
+        for (int bound = gap; bound < array.length;bound++){
+            int v = array[bound];
+            int cur = bound - gap;
+            while (cur >= 0){
+                if (array[cur] > v){
+                    array[cur + gap] = array[cur];
+                }else {
+                    break;
+                }
+                cur -= gap;
+            }
+            array[cur + gap] = v;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,2,51,6,9,10};
+        shellSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
