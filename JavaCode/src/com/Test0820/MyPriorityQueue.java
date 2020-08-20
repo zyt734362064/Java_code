e0a8ef931391202a1ee6894b9e3905ed33ac085e
4ever_11 <734362064@qq.com>
2020-08-21 00:15:26 +0800

    向上调整

--- JavaCode/src/com/Test0820/MyPriorityQueue.java
+++ JavaCode/src/com/Test0820/MyPriorityQueue.java
@@ -9,4 +9,25 @@ package com.Test0820;
 public class MyPriorityQueue {
     private int[] array = new int[100];
     private int size = 0;
+
+    public void offer(int x){
+        array[size] = x;
+        size++;
+        //把新加入的元素进行向上调整
+    }
+    public static void shiftUp(int[] array,int size,int index){
+        int child = index;
+        int parent = (child - 1) / 2;
+        while (child > 0){
+            if (array[child] > array[parent]){
+                int tmp = array[child];
+                array[child] = array[parent];
+                array[parent] = tmp;
+            }else {
+                break;
+            }
+            child = parent;
+            parent = (child - 1) / 2;
+        }
+    }
 }
