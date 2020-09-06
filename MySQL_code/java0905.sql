2dd9b0999f9d0306cc4006c13c527986203aea25
4ever_11 <734362064@qq.com>
2020-09-06 22:02:19 +0800

    update

---
+++ MySQL_code/java0905.sql
@@ -0,0 +1,17 @@
+create table exam_result (
+	id int,
+	name varchar(20),
+	chinese decimal(3,1),
+	math decimal(3,1),
+	english decimal(3,1)
+);
+
+insert into exam_result values
+(8,'关云长',null,null,null);
+(3,'猪八戒',76.5,43.9,69.3),
+(3,'猪八戒',76.5,43.9,69.3),
+(4,'沙和尚',77.9,54.1,91.1),
+(5,'曹孟德',77.5,88.9,77.7),
+(6,'刘玄德',38.2,44.2,69.3),
+(7,'孙仲谋',63.5,79.3,80.5);
+
